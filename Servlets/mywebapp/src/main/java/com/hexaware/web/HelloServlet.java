8ce78405d7780115b1048c1e38f09f00948202f3
javeed512 <javeedmohammedjaveed@gmail.com>
2026-06-11 13:56:38 +0530

    servlets notes , diagram , lifecycle demo

---
+++ Servlets/mywebapp/src/main/java/com/hexaware/web/HelloServlet.java
@@ -0,0 +1,58 @@
+package com.hexaware.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    	
+    	System.out.println("HelloServlet obj created..");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("init() called..");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+
+
+			System.out.println("destroy()  is called..");
+	
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+			System.out.println("service() is called..");
+	
+	}
+
+}
